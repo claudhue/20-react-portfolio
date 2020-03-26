3b962de20f7eb8325644565ffacd19fac9206af2
Francisco Goya <degoyafran@gmail.com>
2020-03-26 09:30:56 -0500

    Updates

---
+++ precache-manifest.8391aa3c6b181b181e623008e3f625f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8aaeb7754b0d9bff5e119c289cbbf3",
+    "url": "/20-react-portfolio/index.html"
+  },
+  {
+    "revision": "bc476134793222a788ff",
+    "url": "/20-react-portfolio/static/css/main.afde38e7.chunk.css"
+  },
+  {
+    "revision": "d5ea38a314b3d9c075ec",
+    "url": "/20-react-portfolio/static/js/2.8cdee0ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/20-react-portfolio/static/js/2.8cdee0ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc476134793222a788ff",
+    "url": "/20-react-portfolio/static/js/main.31226129.chunk.js"
+  },
+  {
+    "revision": "835baabd7bc379c60edf",
+    "url": "/20-react-portfolio/static/js/runtime-main.f425e4f7.js"
+  },
+  {
+    "revision": "1cea294ca57baae3adcf024e197b388e",
+    "url": "/20-react-portfolio/static/media/logo.1cea294c.jpg"
+  },
+  {
+    "revision": "9771e9f1fe08ef018fc64f10aea5280f",
+    "url": "/20-react-portfolio/static/media/logo2.9771e9f1.jpg"
+  }
+]);
